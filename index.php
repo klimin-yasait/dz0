57036dd4ce6b1545917c646d7db87b419d04e56a
klimin-yasait <klimin@yasait.ru>
2019-02-21 02:37:21 +0300

    dz2-2

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
 <?php
 
 require_once ("src/functions.php");
@@ -15,4 +26,20 @@ $myArray = array(
     "Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a,"
 );
 $myBool = FALSE;
-task1($myArray, $myBool);
+//task1($myArray, $myBool);
+
+
+
+$a = '%';
+$b = rand(0,9);
+$c = rand(0,9);
+$d = rand(0,9);
+$e = rand(0,9);
+$f = rand(0,9);
+
+task2($a,$b,$c,$d,$e,$f);
+
+?>
+
+</body>
+</html>
--- src/functions.php
+++ src/functions.php
@@ -1,12 +1,128 @@
 <?php
 
-function task1($myArray,$myBool)
+function task1($myArray, $myBool)
 {
-    foreach ($myArray as &$myArrayString){
+    foreach ($myArray as &$myArrayString) {
         if ($myBool == TRUE) {
             echo $myArrayString;
-        }else{
+        } else {
             echo '<p>' . $myArrayString . '</p>';
         }
     }
-};
+}
+
+;
+
+/**
+ *
+ */
+function task2()
+{
+
+    $numargs = func_num_args();
+    $arg_list = func_get_args();
+
+    $operators = '[+\/*\^%-]';
+    $first_arg = $arg_list[0];
+
+    for ($i = 1; $i < $numargs; $i++) {
+        $el_is_num = is_numeric($arg_list[$i]);
+        if ($el_is_num == false) {
+            echo "Один из операндов не является цифрой!";
+            die;
+        }
+    }
+
+    for ($i = 2; $i < $numargs; $i++) {
+        $is_math = strpos($operators, $first_arg);
+        if ($arg_list[$i] == 0 & $is_math == 3) {
+            echo "На 0 не делим!";
+            die;
+        } elseif ($arg_list[$i] == 0 & $is_math == 7) {
+            echo "На 0 не делим!";
+            die;
+        } elseif ($is_math === false) {
+            echo "Первый аргумент не является оператором!";
+            die;
+        }
+    }
+
+    for ($i = 1; $i < $numargs; $i++) {
+        $current_arg = $arg_list[$i];
+        if ($i > 1) {
+            $el_is_num = is_numeric($arg_list[$i]);
+            if ($el_is_num === true) {
+
+                switch ($is_math) {
+                    case 1:
+                        if ($i != $numargs - 1) {
+                            echo ' + ' . $current_arg;
+                            $summ = $summ + $current_arg;
+                        } else {
+                            echo ' + ' . $current_arg . ' = ';
+                            $summ = $summ + $current_arg;
+                            echo $summ;
+                        }
+                        break;
+                    case 3:
+                        if ($i != $numargs - 1) {
+                            echo ' / ' . $current_arg;
+                            $summ = $summ / $current_arg;
+                        } else {
+                            echo ' / ' . $current_arg . ' = ';
+                            $summ = $summ / $current_arg;
+                            echo $summ;
+                        }
+                        break;
+                    case 4:
+                        if ($i != $numargs - 1) {
+                            echo ' * ' . $current_arg;
+                            $summ = $summ * $current_arg;
+                        } else {
+                            echo ' * ' . $current_arg . ' = ';
+                            $summ = $summ * $current_arg;
+                            echo $summ;
+                        }
+                        break;
+                    case 6:
+                        if ($i != $numargs - 1) {
+                            echo ' ^ ' . $current_arg;
+                            $summ = $summ ^ $current_arg;
+                        } else {
+                            echo ' ^ ' . $current_arg . ' = ';
+                            $summ = $summ ^ $current_arg;
+                            echo $summ;
+                        }
+                        break;
+                    case 7:
+                        if ($i != $numargs - 1) {
+                            echo ' % ' . $current_arg;
+                            $summ = $summ % $current_arg;
+                        } else {
+                            echo ' % ' . $current_arg . ' = ';
+                            $summ = $summ % $current_arg;
+                            echo $summ;
+                        }
+                        break;
+                    case 8:
+                        if ($i != $numargs - 1) {
+                            echo ' - ' . $current_arg;
+                            $summ = $summ - $current_arg;
+                        } else {
+                            echo ' - ' . $current_arg . ' = ';
+                            $summ = $summ - $current_arg;
+                            echo $summ;
+                        }
+                        break;
+                }
+
+            }
+        } else {
+            $summ = $arg_list[$i];
+            echo $summ;
+        }
+    }
+
+}
+
+
