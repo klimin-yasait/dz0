8216424f422454de4bd655de2eb79124068a7508
klimin-yasait <klimin@yasait.ru>
2019-02-26 02:03:51 +0300

    update

---
+++ index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+<h2 style="display: block; width: 100%; text-align: center;">Все домашние задания</h2>
+<ul>
+    <li><a href="dz0/">ДЗ 0</a></li>
+    <li><a href="dz1/">ДЗ 1</a></li>
+    <li><a href="dz2/">ДЗ 2</a></li>
+    <li><a href="dz3/">ДЗ 3</a></li>
+</ul>
+
+</body>
+</html>
